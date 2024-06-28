5e3d64f74e2fba99da02d34113076743955de8ce
diasoy <22082010134@student.upnjatim.ac.id>
2024-06-28 14:16:07 +0700

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="title text-2xl">Home Page</h1>;
+  return (
+    <div className="flex w-full justify-center items-center flex-col h-[100vh] gap-20">
+      <h1 className="text-5xl font-bold">Aplikasi Penyimpanan Contact</h1>
+      <Link
+        href="/contacts"
+        className="bg-blue-600 text-white px-3 py-1 hover:bg-blue-700"
+      >
+        Get Started
+      </Link>
+    </div>
+  );
 }
